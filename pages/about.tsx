0cfe17fe7ef5ee7c9e792e0f4fb150125ac0b0aa
kth4249 <desales4249@gmail.com>
2022-03-28 14:37:05 +0900

    nomadcoders - Start nextJS ~ CSS Modules

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,9 @@
-export default function About() {
-  return <div>About</div>
+import NavBar from './components/NavBar';
+export default function Potato() {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
